3bb92cb66e40b6eb6fd52c041f65f81ddfa026ac
Zhenhuan(Steven) Sun <63125630+sunhuanhuan920@users.noreply.github.com>
2024-06-17 15:12:31 -0400

    Completed implementation of second fourier feature and the corresponding plots.

--- rf.py
+++ rf.py
@@ -6,7 +6,7 @@ def rff_1(X, D):
     """
     :param X: Input data matrix, shape: (N, d)
     :param D: Dimensionality of the randomized feature map
-    :return: Randomized fourier feature map matrix Z, shape: (N, D). Each row of Z corresponds to a randomized fourier
+    :return: Scaled randomized fourier feature map matrix Z, shape: (N, 2D). Each row of Z corresponds to a randomized fourier
     feature map for a data point in X.
     """
     d = X.shape[1]
@@ -15,5 +15,22 @@ def rff_1(X, D):
     cos_features = np.cos(temp)
     sin_features = np.sin(temp)
     Z = np.concatenate((cos_features, sin_features), axis=1)
+
+    return Z / np.sqrt(D)
+
+
+# Second random fourier feature mapping introduced in the paper
+def rff_2(X, D):
+    """
+    :param X: Input data matrix, shape: (N, d)
+    :param D: Dimensionality of the randomized feature map
+    :return: Scaled randomized fourier feature map matrix Z, shape: (N, D). Each row of Z corresponds to a randomized fourier
+    feature map for a data point in X.
+    """
+    N, d = X.shape
+    W = np.random.multivariate_normal(mean=np.zeros(d), cov=np.identity(d), size=D)
+    b = np.random.uniform(0, 2 * np.pi, size=D).reshape(1, -1)
+    temp = X @ W.T + b
+    Z = np.sqrt(2) * np.cos(temp)
 
     return Z / np.sqrt(D)
