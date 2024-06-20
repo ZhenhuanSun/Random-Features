cc288c7d076c030c737984bb9c852640859ccfad
Zhenhuan(Steven) Sun <63125630+sunhuanhuan920@users.noreply.github.com>
2024-06-20 17:36:17 -0400

    Made some progress on the random binning feature.

--- rf.py
+++ rf.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from scipy.stats import rv_continuous
+from collections import defaultdict
 
 # First random fourier feature mapping introduced in the paper
 def rff_1(X, D):
@@ -34,3 +35,46 @@ def rff_2(X, D):
     Z = np.sqrt(2) * np.cos(temp)
 
     return Z / np.sqrt(D)
+
+
+# Gamma distribution arises from laplacian kernel
+class Gamma(rv_continuous):
+    def _pdf(self, delta):
+        return delta * np.exp(-delta)
+
+
+def bin_index(x, delta, u):
+    """
+    :param x: Input vector, shape: (d, ). One row of input data matrix X.
+    :param delta: Grid pitch parameters, shape: (d, ).
+    :param u: Shift parameters, shape: (d, ).
+    :return: An index array that represents the bin, shape: (d, ).
+    """
+    return np.array([np.ceil((x - u) / delta).astype(int)])
+
+
+# Random binning feature mapping (Algorithm 2 in the paper)
+def rbf(X, P):
+    """
+    :param X: Input data matrix, shape: (N, d)
+    :param P: Dimensionality of the randomized feature map
+    :return:
+    """
+    N, d = X.shape
+    Z = np.zeros((N, P))
+    index_matrix = np.zeros((N, d))
+    gamma_dist = Gamma(a=0) # The lower bound of the support of the distribution, i.e., a, is set to 0
+
+    for p in range(P):
+        delta = gamma_dist.rvs(size=d) # Sample d delta from this distribution
+        u = np.random.uniform(0, delta, size=d)
+
+        for i in range(N):
+            index_matrix[i, :] = bin_index(X[i, :], delta, u)
+
+        # Eliminates unoccupied bins from the representation
+
+
+
+
+
