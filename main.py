77e8d22c4f2cf142520ede55b8e9cb0d40d86d2b
Zhenhuan(Steven) Sun <63125630+sunhuanhuan920@users.noreply.github.com>
2024-06-28 16:49:47 -0400

    Deleted figures.ipynb, the same visualizations can now be obtained by running main.py

--- main.py
+++ main.py
@@ -2,47 +2,53 @@ import rf
 import numpy as np
 from sklearn.metrics.pairwise import rbf_kernel, laplacian_kernel
 from matplotlib import pyplot as plt
+from matplotlib.gridspec import GridSpec
 
-x = np.linspace(-5, 5, 100).reshape(-1, 1)
-# Gaussian kernel with \sigma = 1
-gaussian_kernel_exact = rbf_kernel(x, None, 1 / 2)
-# We observe the kernel value of the 50th data point with respect to all other data points, including itself.
-ref = 50
 
-plt.figure(figsize=(14, 8))
-colors = ['red', 'green', 'blue']
-markers = ['o', 's', '^']
-for i, D in enumerate([10, 100, 1000]):
-    Z = rf.rff_1(x, D)
-    gaussian_kernel_approximate = Z @ Z.T
-    plt.plot(x, gaussian_kernel_approximate[ref, :], label=f'rff approximation (D={D})', color=colors[i],
-             linestyle='--', marker=markers[i], markevery=5)
+def plot_comparisons(X, kernel, kernel_name, feature_map, feature_map_name):
+    D_values = [1, 10, 100, 1000]
+    colors = ['red', 'green', 'blue', 'purple']
+    markers = ['o', 's', '^', 'd']
 
-plt.plot(x, gaussian_kernel_exact[ref, :], label='Exact Guassian Kernel', color='black', linewidth=2)
+    fig = plt.figure(figsize=(18, 10))
+    gs = GridSpec(2, 1 + len(D_values), height_ratios=[1, 1], figure=fig)
 
-plt.title('rff_1 approximation of Gaussian Kernel (1D)', fontsize=18)
-plt.xlabel('x', fontsize=14)
-plt.ylabel('Kernel value', fontsize=14)
-plt.legend(loc='best', fontsize=14)
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+    ax1 = fig.add_subplot(gs[0, :])
+    ax1.plot(X, kernel[50, :], label=f'Exact {kernel_name} Kernel', color='black', linewidth=2)
 
-D_values = [1, 10, 100, 1000]
+    ax2 = fig.add_subplot(gs[1, 0])
+    ax2.imshow(kernel)
+    ax2.set_title(f'Exact {kernel_name} Kernel')
+    ax2.set_xlabel('x1')
+    ax2.set_ylabel('x2')
 
-fig, ax = plt.subplots(1, 1 + len(D_values), figsize=(18, 6))
-ax[0].imshow(gaussian_kernel_exact)
-ax[0].set_title(f'Exact Gaussian Kernel', fontsize=14)
-ax[0].set_xlabel('x1', fontsize=14)
-ax[0].set_ylabel('x2', fontsize=14)
+    for i, D in enumerate(D_values):
+        ax = fig.add_subplot(gs[1, i + 1])
+        Z = feature_map(X, D)
+        kernel_approximate = Z @ Z.T
+        ax.imshow(kernel_approximate)
+        ax.set_title(f'{feature_map_name} approximation (D={D})')
+        ax.set_xlabel('x1')
+        ax.set_ylabel('x2')
+        ax1.plot(X, kernel_approximate[50, :], label=f'{feature_map_name} approximation (D={D})', color=colors[i],
+                 linestyle='--', marker=markers[i], markevery=5)
 
-for i, D in enumerate(D_values):
-    Z = rf.rff_1(x, D)
-    gaussian_kernel_approximate = Z @ Z.T
-    ax[i+1].imshow(gaussian_kernel_approximate)
-    ax[i+1].set_title(f'rff_1 approximation (D={D})', fontsize=14)
-    ax[i+1].set_xlabel('x1', fontsize=14)
-    ax[i+1].set_ylabel('x2', fontsize=14)
+    ax1.set_title(f'{feature_map_name} approximation of {kernel_name} Kernel (1D)', fontsize=14)
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('Kernel value')
+    ax1.legend(loc='best')
+    ax1.grid(True)
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+
+
+if __name__ == '__main__':
+    x = np.linspace(-5, 5, 101).reshape(-1, 1)
+    gaussian_kernel = rbf_kernel(x, None, 1/2)  # Gaussian kernel with \sigma = 1
+    laplacian_kernel = laplacian_kernel(x, None, 1)  # Laplacian kernel with gamma = 1
+
+    plot_comparisons(x, gaussian_kernel, 'Gaussian', rf.rff_1, 'rff_1')
+    plot_comparisons(x, gaussian_kernel, 'Gaussian', rf.rff_2, 'rff_2')
+    plot_comparisons(x, laplacian_kernel, 'Laplacian', rf.rbf, 'rbf')
+
+    plt.show()
